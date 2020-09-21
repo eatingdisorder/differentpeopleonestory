02d6f516eaae397ecc3698588ffca59be23abc18
eatingdisorder <71494634+eatingdisorder@users.noreply.github.com>
2020-09-21 04:34:22 +0300

    huge update
    text corrected
    favicon added
    emailjs fixed
    etc

--- script.js
+++ script.js
@@ -2,40 +2,43 @@
 
 /* Quiz */
 function check(){
-  
+
 var question1=document.quiz.question1.value;
 var question2=document.quiz.question2.value;
 var question3=document.quiz.question3.value;
 var correct=0;
 
 
-    if (question1=="Нет"){
+    if (question1=="false"){
         correct++;
   }
 
 
-   if (question2=="Нет"){
+   if (question2=="false"){
        correct++;
-  }  
+  }
 
 
-  if (question3=="Да"){
+  if (question3=="true"){
       correct++;
   }
 
 
-var messages=["Вы очень тактичны в вопросах РПП","Неплохо, но есть над чем работать","Вам нужно проявлять больше эмпатии"];
+var messages=[
+  "Вы проявляете достаточно эмпатии к людям с расстройствами пищевого поведения. Вы можете помочь им еще больше, если донесете их проблему до тех, кто осведомлен об этом в меньшей степени.",
+  "Вы неплохо осведомлены в вопросах РПП. Однако помните, что даже одно неострожное слово, акцент на пище, которую ест человек, или уперк в лени и нежелании изменить свою внешность могут спровоцировать у собеседника приступ голода или булимии. ",
+  "Вам нужно проявлять больше эмпатии к людям с расстройствами пищевого поведения. Даже одно неострожное слово, акцент на пище, которую ест человек, или уперк в лени и нежелании изменить свою внешность могут спровоцировать у собеседника приступ голода или булимии."];
 
 var range;
 
   if(correct<1){
       range=2;
   }
-  
+
   if(correct>0&&correct<3){
       range=1;
   }
-  
+
   if(correct>2){
       range=0;
   }
@@ -45,9 +48,28 @@ document.getElementById("after_submit").style.visibility="visible";
 
 
 document.getElementById("messages").innerHTML=messages[range];
-document.getElementById("number_correct").innerHTML="Коректных ответов"+correct+" из 3";
+document.getElementById("number_correct").innerHTML="Коректных ответов "+correct+" из 3";
 }
 
 
 
+// Feedback form
 
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "gmail";
+  var template_id = "template_QYo1WAau";
+
+  myform.find("button").text("Отправка...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){
+       myform.find("button").text("Отправлено");
+    }, function(err) {
+       alert("Что-то пошло не так");
+       myform.find("button").text("Отправить");
+    });
+  return false;
+});
